0f958bf6643a6ae3a61fa6c971a65503b3f7c5ce
Clare Sudbery <clareworking@claresudbery.co.uk>
2020-10-19 18:33:58 +0100

    move function into a file

--- hello_app.rb
+++ hello_app.rb
@@ -1,9 +1,6 @@
 require 'sinatra'
-
-def day_of_the_week(time)
-    Date::DAYNAMES[time.wday]
-end
+require './day'
 
 get '/' do
-    "Hello, world! Happy #{day_of_the_week(Time.now)}."
+    "Hello, world! Happy #{day_of_the_week(Time.now)}—now from a file!"
 end
